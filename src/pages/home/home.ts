62892c284c6ed4ba364693eddadcf2e14306352e
aviad10m <aviad10m@gmail.com>
2017-08-19 22:13:38 +0300

    Installed cloud-angular an media-capture packages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private mediaCapture: MediaCapture) {
 
   }
 
+  onClickBecomeSheriff() {
+    console.log('clicked');
+    this.mediaCapture.captureImage()
+      .then(
+        (data: MediaFile[]) => console.log(data),
+        (err: CaptureError) => console.error(err)
+      );
+  }
 }

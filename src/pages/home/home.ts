f8b623b3214a9887f424ab45c299ff992bd9df92
Eyal Avidan <avidan.eyal@gmail.com>
2017-08-24 14:32:07 +0300

    camera work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {ServerApiProvider} from "../../providers/server-api/server-api";
+import { ServerApiProvider } from "../../providers/server-api/server-api";
+
+// import { CameraPreview } from "ionic-native";
+
+import { Platform } from 'ionic-angular';
+
+const win = window as any;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +14,27 @@ import {ServerApiProvider} from "../../providers/server-api/server-api";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private serverApi: ServerApiProvider) {
+  rootPage: any = HomePage;
 
+  constructor(public navCtrl: NavController, private serverApi: ServerApiProvider, platform: Platform) {
+
+    platform.ready().then(() => {
+      const CameraPreview = win.CameraPreview;
+
+      let options = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: CameraPreview.CAMERA_DIRECTION.BACK,
+        toBack: true,
+        previewDrag: false
+      };
+
+
+      CameraPreview.stopCamera();
+      CameraPreview.startCamera(options, console.log.bind(console), console.error.bind(console));
+      CameraPreview.takePicture({}, args => alert("win " + args), args => alert("kaka " + args));
+    });
   }
-
 }

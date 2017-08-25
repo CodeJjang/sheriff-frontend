e88f16ee9922765518e387d77e1433c927ee938b
aviad10m <aviad10m@gmail.com>
2017-08-25 07:28:17 +0300

    Deleted page list

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -2,7 +2,6 @@
  * Created by Aviad M on 8/19/2017.
  */
 export * from './home/home';
-export * from './list/list';
 export * from './bounty/bounty';
 export * from './statistics/statistics';
 export * from './about/about';
